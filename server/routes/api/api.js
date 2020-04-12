2d1f4ec649e676c5caa697420671b7ff8d69a729
Victor Juan Jimenez Flores <vj.jimenez96@gmail.com>
2020-04-12 02:28:43 -0500

    updated gui

--- server/routes/api/api.js
+++ server/routes/api/api.js
@@ -116,19 +116,19 @@ router.get("/ieee", async (req, res) => {
   //begin puppeteer
   try {
     var browser = await puppeteer.launch({
-      headless: false
+      headless: true
     });
     const page = await browser.newPage();
     //prevent css and images
-    // await page.setRequestInterception(true);
+    await page.setRequestInterception(true);
 
-    // page.on('request', (req) => {
-    //   if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
-    //     req.abort();
-    //   } else {
-    //     req.continue();
-    //   }
-    // });
+    page.on('request', (req) => {
+      if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
 
     await page.goto(url);
     await timeout(3000);
@@ -147,7 +147,6 @@ router.get("/ieee", async (req, res) => {
       });
       return localResearches;
     });
-    console.log("las investigaciones: ", researches);
   } catch (error) {
     console.log("un error en puppeteer: ", error);
   } finally {
@@ -158,8 +157,71 @@ router.get("/ieee", async (req, res) => {
     researches
   });
 });
-router.post("/goals", goal_controller.goal_create);
+router.post("/ebsco", async (req, res) => {
+  let researches = [];
+  let query = req.body.query;
+  let url = "https://biblioboard.com/opendissertations/";
+  //geting information
+  try {
+    var browser = await puppeteer.launch({
+      headless: true
+    });
+    let page = await browser.newPage();
+    //prevent css and images
+    await page.setRequestInterception(true);
+
+    page.on('request', (req) => {
+      if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
+
+    await page.goto(url);
+    await page.waitForSelector('#ebscohostsearchtext', query);
+    await page.type('#ebscohostsearchtext', query);
+    await page.waitForSelector('.ebscohost-search-button', query);
+    page = await clickAndWaitForTarget('.ebscohost-search-button', page, browser);
+    await page.waitForSelector('.result-list-li');
+    researches = await page.evaluate(() =>
+      Array.from(document.querySelectorAll(".result-list-li")).map((e) =>
+        ({
+          title: e.querySelector(".title-link").innerText,
+          abstract: "",
+          doi: "",
+          link: e.querySelector(".title-link").href,
+          date: e.querySelector('.standard-view-style:nth-child(3)').innerText,
+        })
+      )
+    );
+    console.log("las investigaciones ebsco: ", researches);
+  } catch (error) {
+    console.log("un error en puppeteer ebsco: ", error);
+  } finally {
+    await browser.close();
+  }
+  res.status(200).json({
+    ok: true,
+    researches
+  });
+});
 router.put("/goal/:id", goal_controller.goal_update);
 router.delete("/goal/:id", goal_controller.goal_delete);
+
+//puppeteer functions
+async function clickAndWaitForTarget(clickSelector, page, browser) {
+  const pageTarget = page.target(); //save this to know that this was the opener
+  await page.click(clickSelector); //click on a link
+  const newTarget = await browser.waitForTarget(
+    (target) => target.opener() === pageTarget
+  ); //check that you opened this page, rather than just checking the url
+  const newPage = await newTarget.page(); //get the page object
+  // await newPage.once("load",()=>{}); //this doesn't work; wait till page is loaded
+  // await newPage.waitForSelector("body"); //wait for page to be loaded
+  await timeout(5000);
+  // newPage.on("console", consoleObj => console.log(consoleObj.text()));
+  return newPage;
+}
 
 module.exports = router;
